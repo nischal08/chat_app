1552144d32b28a9c959111b01f88d606b9998890
nischal08 <nischal.karanjit@gmail.com>
2021-06-11 09:29:25 +0545

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
