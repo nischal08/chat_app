8c8fd58ab3cf554cc8ebd1c1c078c3a3b50b3a40
nischal08 <nischal.karanjit@gmail.com>
2021-06-14 16:41:18 +0545

    showing the data of firestore using stream builder and listview

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -6,12 +7,43 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This work'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/LT8eJia23cj0fiJdRUbG/messages')
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          final documents = streamSnapshot.data?.docs;
+          return ListView.builder(
+            itemCount: streamSnapshot.data?.docs.length,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                documents![index]['text'],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/LT8eJia23cj0fiJdRUbG/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((doc) {
+              print(doc['text']);
+            });
+            // print(data.docs[0]['text']);
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
